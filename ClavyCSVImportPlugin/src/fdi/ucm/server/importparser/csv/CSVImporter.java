6fc7765abf2a33fa02c07e5eb14cd06fc8ba3292
Joaquin Gayoso-Cabada <j.gayoso@upm.es>
2022-03-17 12:37:16 +0100

    bien bien

--- ClavyCSVImportPlugin/src/fdi/ucm/server/importparser/csv/CSVImporter.java
+++ ClavyCSVImportPlugin/src/fdi/ucm/server/importparser/csv/CSVImporter.java
@@ -1,16 +1,27 @@
 package fdi.ucm.server.importparser.csv;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
 import fdi.ucm.server.modelComplete.collection.CompleteCollection;
+import fdi.ucm.server.modelComplete.collection.document.CompleteDocuments;
+import fdi.ucm.server.modelComplete.collection.document.CompleteElement;
+import fdi.ucm.server.modelComplete.collection.document.CompleteLinkElement;
+import fdi.ucm.server.modelComplete.collection.document.CompleteTextElement;
+import fdi.ucm.server.modelComplete.collection.grammar.CompleteGrammar;
+import fdi.ucm.server.modelComplete.collection.grammar.CompleteTextElementType;
 
 public class CSVImporter {
 
 	private CompleteCollection CC;
 	private List<String> Logs;
+	public static final String delimiter = ",";
+
 
 	public CSVImporter(List<String> logs) {
 		CC=new CompleteCollection("CSV", "CSV Importdata");
@@ -18,19 +29,63 @@ public class CSVImporter {
 	}
 
 	public void ProcessFile(String fileCSV) {
+		
+		
 		try {
-			 Scanner sc = new Scanner(new File(fileCSV));
-			    //parsing a CSV file into the constructor of Scanner class 
-			    sc.useDelimiter(",");
-			    //setting comma as delimiter pattern
-			    while (sc.hasNext()) {
-			      System.out.print(sc.next());
-			    }
-			    sc.close();
-			    //closes the scanner  
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		      File file = new File(fileCSV);
+		      
+		      CompleteGrammar G=new CompleteGrammar(file.getName(), file.getName(), CC);
+		      CC.getMetamodelGrammar().add(G);
+		      
+		      FileReader fr = new FileReader(file);
+		      BufferedReader br = new BufferedReader(fr);
+		      String line = " ";
+		      String[] tempArr;
+		      boolean PrimeraCategoria=true;
+		      LinkedList<CompleteTextElementType> listaElementos=new LinkedList<CompleteTextElementType>();
+		      int docuemntoConteo=1;
+		      while ((line = br.readLine()) != null) {
+		        tempArr = line.split(delimiter);
+		        
+		        CompleteDocuments CD=null;
+		        if (!PrimeraCategoria)
+		        	{
+		        	CD=new CompleteDocuments(CC,"Documento ->"+docuemntoConteo++,"");
+	        		CC.getEstructuras().add(CD);
+		        	}
+		        
+		        for (int i = 0; i < tempArr.length; i++) {
+		        	String tempStr=tempArr[i]; 
+		        	
+		        	if (PrimeraCategoria)
+		        	{
+		        	CompleteTextElementType textValue=new CompleteTextElementType(tempStr, G);
+		        	G.getSons().add(textValue);
+		        	
+		        	listaElementos.add(i, textValue);
+		        	}else
+		        	{
+		        		
+		        		CompleteTextElementType Estees=listaElementos.get(i);
+		        		CD.getDescription().add(new CompleteTextElement(Estees, tempStr));
+		        	}
+		        	
+		        	
+		        	
+		          
+		        }
+		        
+		        if (PrimeraCategoria)
+		        	PrimeraCategoria=false;
+//		        else
+//		        	System.out.println();
+		      }
+		      br.close();
+		    }
+		    catch(IOException ioe) {
+		      ioe.printStackTrace();
+		      Logs.add(ioe.getMessage());
+		    }
 		
 		
 	}
@@ -42,7 +97,18 @@ public class CSVImporter {
 	
 	public static void main(String[] args) {
 		LinkedList<String> SS=new LinkedList<String>();
-		new CSVImporter(SS).ProcessFile("/ejemplo/ejemplo.csv");
+		CSVImporter csVimpo=new CSVImporter(SS);
+		csVimpo.ProcessFile("ejemplo/ejemplo.csv");
+		CompleteCollection CC= csVimpo.getCollection();
+		
+		for (CompleteDocuments docume : CC.getEstructuras()) {
+			System.out.println("docu"+docume.getDescriptionText());
+			for (CompleteElement eleme : docume.getDescription()) {
+				System.out.println("-----"+eleme.getHastype().getName()+
+						":"+((CompleteTextElement)eleme).getValue());
+			}
+		}
+		
 		for (String string : SS) {
 			System.err.println(string);
 		}
